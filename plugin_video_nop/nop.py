fa74632246ea519dfc89e1a1f4375a3da06b38da
docee <docee@163.com>
2016-01-11 00:09:43 +0800

    完成视频获取与播放

--- plugin_video_nop/nop.py
+++ plugin_video_nop/nop.py
@@ -9,6 +9,7 @@ import sys
 from nopcore import NOPCore
 import xbmcplugin
 import xbmcgui
+import xbmc
 from urlparse import parse_qsl
 
 _url = sys.argv[0]
@@ -17,7 +18,7 @@ _handle = int(sys.argv[1])
 CATEGORYS = ["最近得分","收藏最多","最近加精","本月最热","上月最热"]
 
 # 初始化NOP服务器地址
-core = NOPCore("======服务器地址保密=======")
+core = NOPCore("nop.skyworth.wang")
 
 
 # 列出视频类别
@@ -29,12 +30,12 @@ def list_category(categorys):
 
         list_item = xbmcgui.ListItem(label=categorys[i])
 
-        url = '{0}?action=list&category={1}'.format(_url,i)
+        url = '{0}?action=list&category={1}&page=1'.format(_url,i)
 
         lists.append((url,list_item,True))
 
     xbmcplugin.addDirectoryItems(_handle, lists, len(lists))
-    xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+    # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
     xbmcplugin.endOfDirectory(_handle)
 
 # 列出视频列表
@@ -42,11 +43,53 @@ def list_category_videos(categoryIndex,page):
 
     try:
         result = core.requestVideoList(categoryIndex,page)
+
+        videos = result['videos']
+
+        lists = []
+
+        for i in range(0,len(videos)):
+            title = videos[i]['title']
+
+            duration_strings = videos[i]['duration'].split(':')
+
+            duration = int(duration_strings[0]) * 60 + int(duration_strings[1])
+
+            list_item = xbmcgui.ListItem(label=title,thumbnailImage=videos[i]['screenshot'][0])
+            list_item.setInfo('video',{'title':title,'duration':duration})
+            list_item.setProperty('IsPlayable','true')
+
+            url = '{0}?action=viewkey&viewkey={1}'.format(_url,videos[i]['viewkey'])
+
+            lists.append((url,list_item,False))
+
+        xbmcplugin.addDirectoryItems(_handle, lists, len(lists))
+        # xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
+        xbmcplugin.endOfDirectory(_handle)
+
     except:
 
-        #TODO
+        #TODO 获取视频失败异常处理(暂时不处理)
         pass
 
+
+# 获取视频地址并播放
+def get_video(viewkey):
+
+    try:
+        result = core.requestVideo(viewkey)
+
+        url = result['url']
+
+        play_item = xbmcgui.ListItem(path=url)
+
+        xbmcplugin.setResolvedUrl(_handle,True,listitem=play_item)
+        # xbmc.Player.play(item=play_item)
+
+
+    except:
+        #TODO 播放视频失败异常处理(暂时不处理)
+        pass
 
 
 # 路由
@@ -57,36 +100,20 @@ def router(paramstring):
 
     if params:
 
+        # 获取视频列表
         if params['action'] == 'list':
-            #TODO
             category = int(params['category'])
+            page = int(params['page'])
 
-            result = core.requestVideoList(category,1)
-            videos = result['videos']
+            list_category_videos(category,page)
 
-            lists = []
-
-            for i in range(0,len(videos)):
-                title = videos[i]['title']
-                list_item = xbmcgui.ListItem(label=title)
-
-                url = '{0}?action=viewkey&viewkey={1}'.format(_url,videos[i]['viewkey'])
-
-                lists.append((url,list_item,True))
-
-            xbmcplugin.addDirectoryItems(_handle, lists, len(lists))
-            xbmcplugin.addSortMethod(_handle, xbmcplugin.SORT_METHOD_LABEL_IGNORE_THE)
-            xbmcplugin.endOfDirectory(_handle)
-
-
-
+        # 获取视频播放地址
         elif params['action'] == 'viewkey':
-            #TODO
-            pass
-        elif params['action'] == 'play':
-            #TODO
-            pass
+            viewkey = params['viewkey']
+            get_video(viewkey)
+
     else:
+        # 获取视频分类
         list_category(CATEGORYS)
 
 # 程序主入口
